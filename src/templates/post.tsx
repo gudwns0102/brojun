c227dbbde7125577f7f563a44e6cca728be49502
김형준 <hyungjun.kim@cosmochain.io>
2019-04-20 23:08:13 +0900

    update

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -2,19 +2,60 @@ import React from "react";
 import styled from "styled-components";
 import { PageContainer } from "../components/layouts/PageContainer";
 import { Text } from "../components/texts/Text";
+import { colors } from "../styles/colors";
 
 const Container = styled(PageContainer)``;
 
-const Title = styled(Text)``;
+const Header = styled.div<{ backgroundImage?: IPost["backgroundImage"] }>`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 360px;
 
-const Content = styled(Text)``;
+  &::before {
+    background-image: url("${props => props.backgroundImage}");
+    background-size: cover;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+    opacity: 0.8;
+  }
+`;
+
+const Title = styled(Text).attrs({ fontType: "bold" })`
+  display: block;
+  font-size: 48px;
+  color: ${colors.gray20};
+`;
+
+const Subtitle = styled(Text).attrs({ fontType: "medium" })`
+  font-size: 32px;
+  color: ${colors.gray20};
+`;
+
+const Body = styled.div``;
+
+const Content = styled(Text).attrs({ fontType: "regular" })`
+  font-size: 20px;
+`;
 
 export default (props: any) => {
-  const { post } = props.pageContext;
+  const { post }: { post: IPost } = props.pageContext;
+  const { backgroundImage, title, content } = post;
+
   return (
     <Container>
-      <Title>{post.title}</Title>
-      <Content>{post.content}</Content>
+      <Header backgroundImage={backgroundImage}>
+        <Title>{title}</Title>
+        <Subtitle>Subtitle</Subtitle>
+      </Header>
+      <Body>
+        <Content>{content}</Content>
+      </Body>
     </Container>
   );
 };
---
+++ typings/Post.d.ts
@@ -0,0 +1,7 @@
+interface IPost {
+  id: number;
+  title: string;
+  subtitle?: string;
+  content: string;
+  backgroundImage?: string;
+}
